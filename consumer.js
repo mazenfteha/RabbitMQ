7ddb796a14d1ac07a2850ee3f08d33940ab9f16f
mazenfteha <mazn34903@gmail.com>
2023-08-27 23:27:04 -0700

    write a consumer client

--- consumer.js
+++ consumer.js
@@ -1,6 +1,5 @@
 const amqp = require("amqplib")
 
-const msg = {number : process.argv[2]}
 
 connect()
 async function connect() {
@@ -8,9 +7,13 @@ async function connect() {
         const connection = await amqp.connect("amqp://localhost:5672");
         const channel = await connection.createChannel();
         const result = channel.assertQueue("jobs");
-        channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
-        console.log(`job sent successfully ${msg.number}`)
 
+        channel.consume("jobs", message => {
+            const input = JSON.parse(message.content.toString())
+            console.log(`Recieved job with input ${input.number}`)
+        })
+        
+        console.log("Wating for message...")
     } catch (error) {
         console.log(error)
     }
